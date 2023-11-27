ca4297388dd9995cb78bda86150944b8e0a0dadc
se22630 <m314will@users.noreply.github.com>
2023-11-27 14:18:21 +0000

    Makecall added + comment

--- broker/broker.go
+++ broker/broker.go
@@ -1,8 +1,22 @@
 package main
 
+import (
+	"net/rpc"
+	"uk.ac.bris.cs/gameoflife/stubs"
+)
+
 var Tchan int
 var Pause bool
 var World [][]byte
+
+func makeCall(client *rpc.Client, world [][]byte, p Params) *stubs.Response) {
+	request := stubs.Request{StartY: 0, EndY: p.ImageHeight, StartX:0, EndX: p.ImageWidth, World: world, Turns: p.Turns}    response := new(stubs.Response)
+	response := new(stubs.Response)
+	//I think will need a new stubs to pass appropriate values. Also not sure if all params are needed, + start x is useless even when parallelised
+	return response
+}
+
+
 
 func main() {
 
